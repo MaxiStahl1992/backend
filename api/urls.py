91c2a86a9857e64c3171d04022b8869e852fafb7
Max <stahlma@baywa.de>
2024-11-02 11:18:40 +0100

    change route names

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.redirect_to_frontend, name="redirect_to_frontend"),
     path("app/", views.redirect_to_frontend, name="redirect_to_frontend"),
-    path("generate-response/", views.generate_response, name="generate_response"),
-    path("check-authentication/", views.check_authentication, name="check_authentication"),
+    path("api/generate-response/", views.generate_response, name="generate_response"),
+    path("api/check-authentication/", views.check_authentication, name="check_authentication"),
 ]
